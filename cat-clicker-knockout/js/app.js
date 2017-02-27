cc1cb8ab6f4ec998ae207a659f68229ec0e5edae
Carlos Abdalla <abdalla@msdev.com.br>
2017-02-26 22:33:22 -0500

    list of cats

--- cat-clicker-knockout/js/app.js
+++ cat-clicker-knockout/js/app.js
@@ -1,8 +1,45 @@
-let Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Mussum');
-    this.imgSrc = ko.observable('http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg');
-    this.nicknames = ko.observableArray(['Gordo', 'Zigodi', 'Obeso', 'Mussumzera', 'Negoncio'])
+let initialData = [
+                {
+                    id: 1,
+                    clickCount: 0,
+                    name: 'Mussum',
+                    imgSrc: 'http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg',
+                    nicknames: ['Gordo', 'Zigodi', 'Obeso', 'Mussumzera', 'Negoncio']
+                },
+                {
+                    id: 2,
+                    clickCount: 0,
+                    name: 'Cacildis',
+                    imgSrc: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+                    nicknames: ['Gorda']
+                },
+                {
+                    id: 3,
+                    clickCount: 0,
+                    name: 'Chiloe',
+                    imgSrc: 'https://www.wired.com/wp-content/uploads/2014/10/cat-ft.jpg',
+                    nicknames: ['Jurupinga']
+                },
+                {
+                    id: 4,
+                    clickCount: 0,
+                    name: 'Mike',
+                    imgSrc: 'http://i2.cdn.cnn.com/cnnnext/dam/assets/150324154025-14-internet-cats-restricted-super-169.jpeg',
+                    nicknames: ['Jurupingis']
+                },
+                {
+                    id: 5,
+                    clickCount: 0,
+                    name: 'Madona',
+                    imgSrc: 'https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-06.jpg',
+                    nicknames: ['Mais loka que o batman']
+                }
+];
+let Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.nicknames = ko.observableArray(data.nicknames)
     this.title = ko.computed(function() {
         if (this.clickCount() <= 10) {
             return 'Newborn';
@@ -15,11 +52,23 @@ let Cat = function() {
 }
 
 let VierModel = function() {
-    this.cat = ko.observable( new Cat() );
+    const self = this;
+
+    this.cats = ko.observableArray([]);
+
+    initialData.forEach(cat => self.cats.push(new Cat(cat)));
+
+    this.cat = ko.observable( self.cats()[0] );
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
+        //or
+        //self.cat().clickCount(self.cat().clickCount() + 1);
     };
+
+    this.setCurrentCat = function(selectedCat) {
+        self.cat(selectedCat);
+    }
 };
 
 ko.applyBindings(new VierModel());
